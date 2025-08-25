f681086ae3947a829c5c1a371b049c5a75a20cb1
Sylvye <thpexil@gmail.com>
2025-08-24 21:00:38 -0400

    Added zooming to map
    VERY BUGGY TREAD LIGHTLY

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,6 +8,8 @@ public static class CameraManager
         map
     }
 
+    private static float prevTimeScale;
+
     public static Camera mainCamera = MainCameraController.main.GetComponent<Camera>();
     public static Camera mapCamera = MapCameraController.main.GetComponent<Camera>();
     public static CameraType cameraType = CameraType.main;
@@ -19,11 +21,14 @@ public static class CameraManager
         {
             mainCamera.enabled = true;
             mapCamera.enabled = false;
+            prevTimeScale = Time.timeScale;
+            Time.timeScale = 0;
         }
         else
         {
             mainCamera.enabled = false;
             mapCamera.enabled = true;
+            Time.timeScale = prevTimeScale;
         }
     }
 
@@ -33,11 +38,16 @@ public static class CameraManager
         {
             mainCamera.enabled = false;
             mapCamera.enabled = true;
+            cameraType = CameraType.map;
+            prevTimeScale = Time.timeScale;
+            Time.timeScale = 0;
         }
         else
         {
             mainCamera.enabled = true;
             mapCamera.enabled = false;
+            cameraType = CameraType.main;
+            Time.timeScale = prevTimeScale;
         }
     }
 }
--- Assets/Scripts/MapCameraController.cs
+++ Assets/Scripts/MapCameraController.cs
@@ -1,16 +1,53 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class MapCameraController : MonoBehaviour
 {
     public static MapCameraController main;
+    public float zoomSpeed;
+    private PlayerInputActions inputActions;
+    private float zoomAmount = 1;
+    private PixelPerfectCamera ppc;
+    private Vector2 refRes;
+    private GameObject grid;
+    private Vector3 gridOriginalScale;
 
     private void Awake()
     {
         main = this;
+        ppc = GetComponent<PixelPerfectCamera>();
+        inputActions = new PlayerInputActions();
+        refRes = new(ppc.refResolutionX, ppc.refResolutionY);
+        grid = GameObject.Find("Grid");
+        gridOriginalScale = grid.transform.localScale;
     }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.UI.ScrollWheel.performed += scrl => zoomAmount += scrl.ReadValue<Vector2>().y * zoomSpeed;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     private void Update()
     {
-        transform.position = new Vector3(PlayerController.main.transform.position.x, PlayerController.main.transform.position.y, -100);
+        switch (CameraManager.cameraType)
+        {
+            case CameraManager.CameraType.main:
+                transform.position = new Vector3(PlayerController.main.transform.position.x, PlayerController.main.transform.position.y, -100);
+                break;
+            case CameraManager.CameraType.map:
+                ppc.refResolutionX = (int)(refRes.x * zoomAmount);
+                ppc.refResolutionY = (int)(refRes.y * zoomAmount);
+                grid.transform.localScale = gridOriginalScale * zoomAmount;
+                // TEMP: update grid scale value here
+                Debug.Log(zoomAmount);
+                break;
+        }
     }
 }
